638845fa6ac40cf0adc0f7ee798a3b469f78ed41
Javier Sanz <javiersanzlozano@gmail.com>
2024-07-17 12:55:17 +0200

    changes from local

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ let totalCena = 120
 let totalBebidas = 18
 let cuantoPagaIndividualmenteCadaComensal = (totalCena - totalBebidas)/6
 let lasBebidasCorrenDeMiCuenta = totalBebidas + cuantoPagaIndividualmenteCadaComensal
- 
+  
 console.log(`Cada comensal pagará ${cuantoPagaIndividualmenteCadaComensal}`)
 console.log(`Yo pagaré mi parte ${cuantoPagaIndividualmenteCadaComensal} y las bebidas, haciendo un total de ${lasBebidasCorrenDeMiCuenta}`)
 
