b78b7606c7ba5769c81d802b32cea104bd3eb278
Javier Sanz <javiersanzlozano@gmail.com>
2024-08-14 21:00:09 +0200

    changes from local

--- modulo-4/src/main.ts
+++ modulo-4/src/main.ts
@@ -15,21 +15,29 @@ if (resetButton != undefined && nextButton != null){
     resetButton.addEventListener('click', resetCount)
 }
 
+let newCurrentValueCounter = document.getElementById("currentValueCounter")
+
 function renderNextCount() {
     currentValueCounter += 1
-    document.getElementById("currentValueCounter").innerHTML = currentValueCounter.toString().padStart(2, '0')
+    if (newCurrentValueCounter != null && newCurrentValueCounter != undefined){
+        newCurrentValueCounter.innerHTML = currentValueCounter.toString().padStart(2, '0')
+    }
     return currentValueCounter
 }
 
 function renderPreviousCount() {
     currentValueCounter -= 1
-    document.getElementById("currentValueCounter").innerHTML = currentValueCounter.toString().padStart(2, '0')
+    if (newCurrentValueCounter != null && newCurrentValueCounter != undefined){
+        newCurrentValueCounter.innerHTML = currentValueCounter.toString().padStart(2, '0')
+    }
     return currentValueCounter
 }
 
 function resetCount() {
     currentValueCounter = 0
-    document.getElementById("currentValueCounter").innerHTML = currentValueCounter.toString().padStart(2, '0')
+    if (newCurrentValueCounter != null && newCurrentValueCounter != undefined){
+        newCurrentValueCounter.innerHTML = currentValueCounter.toString().padStart(2, '0')
+    }
     return currentValueCounter
 }
 
@@ -39,14 +47,10 @@ let skipNumber = document.getElementById("skipNumber") as HTMLInputElement
 skipNumber.addEventListener('input', monitorSkip)
 
 function monitorSkip() {
-    document.getElementById("currentValueCounter").innerHTML = skipNumber.value
+    if (newCurrentValueCounter != null && newCurrentValueCounter != undefined){
+        newCurrentValueCounter.innerHTML = skipNumber.value
+    }
     currentValueCounter = parseInt(skipNumber.value)
 }
 
-// implement padder
-
-// to do
-/*
-* enable strict and fix things
-*/
 
